72b62a76b69f8669611056479fe4fe979d4cd9a9
Steeven <steeveric5@gmail.com>
2026-07-03 12:47:53 +0300

    fix(home): Change to equipements

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-
-$routes->get('equipements', 'Equipements::index');
+$routes->get('/', 'Equipements::index');
 $routes->get('equipements/new', 'Equipements::new');
 $routes->post('equipements/create', 'Equipements::create');
